998df6113ec31e308a9c296b26df77f04fd77721
emirdelic <emir.delic@productdock.com>
2026-05-13 11:48:13 +0200

    Implement PolyAPI SDK

--- main.tf
+++ main.tf
@@ -18,16 +18,27 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# --- Root Variables ---
+
 variable "aws_account_id" {
   type      = string
   sensitive = true
 }
+
 variable "poly_api_key" {
   type      = string
   sensitive = true
 }
 
+# This captures the timestamped zip name from the GitHub Action -var flag
+variable "lambda_zip_path" {
+  type        = string
+  description = "The dynamic name of the generated Lambda deployment package"
+  default     = "optimizer.zip"
+}
+
 # --- Module Orchestration ---
+
 module "api" {
   source     = "./aws/api"
   lambda_uri = module.green_logistics_lambda.invoke_arn 
@@ -39,7 +50,12 @@ module "green_logistics_lambda" {
   api_id           = module.api.api_id
   root_resource_id = module.api.root_resource_id
   poly_api_key     = var.poly_api_key
+  
+  # Passing the zip path down to the module
+  lambda_zip_path  = var.lambda_zip_path
 }
+
+# --- Outputs ---
 
 output "final_api_endpoint" {
   value       = module.api.invoke_url
